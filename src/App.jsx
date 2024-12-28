389bf524ebab1e0a3d854cc148ab875f6c6187ab
jctech111 <jctech333@gmail.com>
2024-12-28 14:27:25 +0100

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import AppRouter from './routes/AppRouter'
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 import './App.css'
 
 function App() {
   return (
+    <>
     <AppRouter />
+    <SpeedInsights />
+    <Analytics />
+    </>
   )
 }
 
