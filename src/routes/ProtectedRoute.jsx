e98e499689dfc60630801bffd1c437be1b54ee8b
jctech111 <jctech333@gmail.com>
2024-12-30 15:09:08 +0100

    working on the dashboard page

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
+import SoundWaveLoader from '../components/SoundWaveLoader';
 
 const ProtectedRoute = ({ children }) => {
   const { userId, loading } = useContext(AuthContext);
 
   // If the loading state is still true, show a loading indicator
   if (loading) {
-    return <div>Loading...</div>;
+    return <div className='flex items-center justify-center w-screen h-screen'><SoundWaveLoader /></div>;
   }
 
   // If user is authenticated (userId exists), render the children (protected content)
