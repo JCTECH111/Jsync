14f07e2776c743696aa1bd5c2cd36d0c4fea2ded
jctech111 <jctech333@gmail.com>
2025-01-09 23:42:56 +0100

    working on the share actions

--- src/landing/publicView.jsx
+++ src/landing/publicView.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useContext } from "react";
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 import { useParams } from "react-router-dom";
 import { Databases, Storage } from "appwrite";
 import { client } from "../lib/appwrite";
