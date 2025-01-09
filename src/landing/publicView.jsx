9bb2ae01a8ffd0c522fb32538b9ce4d64515e167
jctech111 <jctech333@gmail.com>
2025-01-09 22:30:22 +0100

    working on the share actions

--- src/landing/publicView.jsx
+++ src/landing/publicView.jsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import { Helmet } from "react-helmet";
 import { useParams } from "react-router-dom";
 import { Databases } from "appwrite";
 import { client } from "../lib/appwrite";
@@ -12,26 +13,34 @@ const PublicView = () => {
 
   useEffect(() => {
     const fetchFileData = async () => {
-    //   try {
-    //     const response = await databases.getDocument(
-    //       "databaseId", // Replace with your Appwrite database ID
-    //       "collectionId", // Replace with your Appwrite collection ID
-    //       id
-    //     );
-    //     setFileData(response);
-    //   } catch (error) {
-    //     console.error("Error fetching file data:", error);
-    //   }
+      try {
+        const response = await databases.getDocument(
+          "databaseId", // Replace with your Appwrite database ID
+          "collectionId", // Replace with your Appwrite collection ID
+          id
+        );
+        setFileData(response);
+      } catch (error) {
+        console.error("Error fetching file data:", error);
+      }
 
     };
 
     fetchFileData();
   }, [id]);
 
-  if (!fileData) return <div className="w-full h-screen items-center justify-center"><SoundWaveLoader /></div>;
+  if (!fileData) return <div className="w-full flex h-screen items-center justify-center"><SoundWaveLoader /></div>;
 
   return (
     <div>
+    <Helmet>
+      <title>{fileData.metadata.title}</title>
+      <meta name="description" content={fileData.metadata.description} />
+      <meta property="og:title" content={fileData.metadata.title} />
+      <meta property="og:description" content={fileData.metadata.description} />
+      <meta property="og:image" content="path-to-thumbnail" />
+      <meta property="og:url" content={`https://your-domain.com/view/${fileData.$id}`} />
+    </Helmet>
       <h1>{fileData.metadata.title}</h1>
       <p>{fileData.metadata.description}</p>
       <iframe
