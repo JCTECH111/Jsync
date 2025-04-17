21b014815de604077f935a882f60bbd6036c51da
jctech111 <jctech333@gmail.com>
2025-04-17 22:47:36 +0100

    working on the searched item user icon

--- src/Dashboard/Search.jsx
+++ src/Dashboard/Search.jsx
@@ -191,11 +191,11 @@ const SearchPage = () => {
                   className="h-[5rem] transition-colors hover:bg-gray-100 whitespace-nowrap"
                 >
                   {/* Image Column */}
-                  <td className="w-[4rem] px-6 py-4 whitespace-nowrap">
+                  <td className="w-[5rem]  px-6 py-4 whitespace-nowrap">
                     <img
-                      src={doc.user.profile}
+                      src="https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.8Ck3Lp06jASLSybjpchJJgHaHa%26pid%3DApi&sp=1744926284T6f7f329677ddd2a100f7e5c10f8ee6f561f4c0a8a620edb23245bf8b9768e295"            //{doc.user.profile}
                       alt="Profile"
-                      className="w-[3rem] h-[3rem] object-cover rounded-full border border-gray-300"
+                      className="w-[5rem] h-[3rem] object-contain rounded-full border border-gray-300"
                     />
                   </td>
 
